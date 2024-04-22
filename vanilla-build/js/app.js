8aed8252d3d69db7e2591bc2fdc99663014b7614
Sheldon <kasebrot@live.com>
2024-04-22 16:54:31 -0500

    Created data-id for selectors and...
    I hooked up the reset and new round buttons.

--- vanilla-build/js/app.js
+++ vanilla-build/js/app.js
@@ -4,14 +4,24 @@ console.log("hello, there");
 const App = {
     // All of our selected HTML elements
     $: {
-        menu: document.querySelector(".menu"),
-        menuItems: document.querySelector(".items"),
+        menu: document.querySelector('[data-id="menu"]'),
+        menuItems: document.querySelector('[data-id="menu-items"]'),
+        resetBtn: document.querySelector('[data-id="reset-btn"]'),
+        newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
     },
 
     init() {
         App.$.menu.addEventListener("click", (event) => {
             App.$.menuItems.classList.toggle('hidden');
-        })
+        });
+
+        App.$.resetBtn.addEventListener('click', event =>{
+            console.log("Reset the game")
+        });
+
+        App.$.newRoundBtn.addEventListener('click', event =>{
+            console.log("Start the next round")
+        });
     },
 };
 
