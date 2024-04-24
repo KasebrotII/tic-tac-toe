40bf3e69d45a37534599bb28494d130eddeb6598
Sheldon <kasebrot@live.com>
2024-04-24 16:37:14 -0500

    Clicking on a square now changes the ui...
    to match the current players icon.

--- vanilla-build/js/app.js
+++ vanilla-build/js/app.js
@@ -11,6 +11,10 @@ const App = {
         squares: document.querySelectorAll('[data-id="square"]'),
     },
 
+    state: {
+        currentPlayer: 1,
+    },
+
     init() {
 
         App.registerEventListeners()
@@ -20,7 +24,7 @@ const App = {
 
     registerEventListeners() {
 
-        App.$.menu.addEventListener("click", (event) => {
+        App.$.menu.addEventListener('click', (event) => {
             App.$.menuItems.classList.toggle('hidden');
         });
 
@@ -35,6 +39,26 @@ const App = {
         App.$.squares.forEach(square => {
             square.addEventListener('click', event => {
                 console.log(`Square with id ${event.target.id} was clicked`);
+                console.log(`Current player is ${App.state.currentPlayer}`);
+
+                if (square.hasChildNodes()) {
+                    return;
+                }
+
+                const currentPlayer = App.state.currentPlayer;
+
+                const icon = document.createElement('i');
+
+                if (currentPlayer === 1) {
+                    icon.classList.add('fa-solid', 'fa-x', 'yellow');
+                } else {
+                    icon.classList.add('fa-solid', 'fa-o', 'turquoise');
+                }
+
+                App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+
+                square.replaceChildren(icon);
+
             });
         });
     }
