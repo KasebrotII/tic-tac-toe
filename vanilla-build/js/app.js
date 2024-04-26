8ce35e09b2b7a3f0747f2f158161c7ae9470182f
Sheldon <kasebrot@live.com>
2024-04-26 16:45:52 -0500

    checking to see if a players moves match any winning pattern.

--- vanilla-build/js/app.js
+++ vanilla-build/js/app.js
@@ -1,6 +1,7 @@
 console.log("hello, there");
 
 
+
 const App = {
     // All of our selected HTML elements
     $: {
@@ -13,6 +14,38 @@ const App = {
 
     state: {
         moves: [],
+    },
+
+    getGamesStatus(moves) {
+
+        const p1Moves = moves.filter(move => move.playerId === 1).map(move => +move.squareId)
+        const p2Moves = moves.filter(move => move.playerId === 2).map(move => +move.squareId)
+
+        const winningPatterns = [
+            [1, 2, 3],
+            [1, 5, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 5, 7],
+            [3, 6, 9],
+            [4, 5, 6],
+            [7, 8, 9],
+          ];
+
+          let winner = null
+          winningPatterns.forEach(pattern => {
+            const p1Wins = pattern.every(v => p1Moves.includes(v))
+            const p2Wins = pattern.every(v => p2Moves.includes(v))
+
+            if (p1Wins) winner = 1
+            if (p2Wins) winner = 2
+
+          });
+
+        return {
+            status: moves.length === 9 || winner != null ? 'complete' : 'in-progress', // in-progress | complete
+            winner // 1 | 2 | null
+        }
     },
 
     init() {
@@ -69,25 +102,12 @@ const App = {
                     playerId: currentPlayer
                 });
 
-                App.state.currentPlayer = currentPlayer === 1 ? 2 : 1;
-
-                console.log(App.state)
-
-
-
                 square.replaceChildren(icon);
 
                 // Check to see if there is a winner or a tie game.
-                const winningPatterns = [
-                    [1, 2, 3],
-                    [1, 5, 9],
-                    [1, 4, 7],
-                    [2, 5, 8],
-                    [3, 5, 7],
-                    [3, 6, 9],
-                    [4, 5, 6],
-                    [7, 8, 9],
-                  ];
+                const status = App.getGamesStatus(App.state.moves)
+
+                console.log(status)
             });
         });
     }
