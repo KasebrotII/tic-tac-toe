e84a24b6fc9ad4f53b9f41f4bfcb62c5edaa0532
Sheldon <kasebrot@live.com>
2024-04-28 16:55:18 -0500

    Updated the label to also show the players corresponding color.

--- vanilla-build/js/app.js
+++ vanilla-build/js/app.js
@@ -100,24 +100,29 @@ const App = {
                 : getOppositePlayer(lastMove.playerId);
                 const nextPlayer = getOppositePlayer(currentPlayer);
 
-                const icon = document.createElement('i');
+                const squareIcon = document.createElement('i');
+                const turnIcon = document.createElement('i');
                 const turnLabel = document.createElement('p');
-                turnLabel.innerText = `Player ${nextPlayer}, you are up!`
+                turnLabel.innerText = `Player ${nextPlayer}, you are up!`;
 
                 if (currentPlayer === 1) {
-                    icon.classList.add('fa-solid', 'fa-x', 'yellow');
+                    squareIcon.classList.add('fa-solid', 'fa-x', 'yellow');
+                    turnIcon.classList.add('fa-solid', 'fa-o', 'turquoise');
+                    turnLabel.classList = 'turquoise'
                 } else {
-                    icon.classList.add('fa-solid', 'fa-o', 'turquoise');
+                    squareIcon.classList.add('fa-solid', 'fa-o', 'turquoise');
+                    turnIcon.classList.add('fa-solid', 'fa-x', 'yellow');
+                    turnLabel.classList = 'yellow'
                 }
 
-                App.$.turn.replaceChildren(icon, turnLabel);
+                App.$.turn.replaceChildren(turnIcon, turnLabel);
 
                 App.state.moves.push({
                     squareId: +square.id,
                     playerId: currentPlayer
                 });
 
-                square.replaceChildren(icon);
+                square.replaceChildren(squareIcon);
 
                 // Check to see if there is a winner or a tie game.
                 const game = App.getGamesStatus(App.state.moves)
